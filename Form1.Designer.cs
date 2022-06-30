c719374081d2322316c706c66e156b93186da793
Ivan Fakoff <ifakov239@gmail.com>
2022-06-30 19:15:46 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace dop5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_a1 = new System.Windows.Forms.Button();
+            this.b_a2 = new System.Windows.Forms.Button();
+            this.b_a3 = new System.Windows.Forms.Button();
+            this.b_b1 = new System.Windows.Forms.Button();
+            this.b_b2 = new System.Windows.Forms.Button();
+            this.b_b3 = new System.Windows.Forms.Button();
+            this.b_c1 = new System.Windows.Forms.Button();
+            this.b_c2 = new System.Windows.Forms.Button();
+            this.b_c3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.b_a10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b_a1
+            // 
+            this.b_a1.BackColor = System.Drawing.SystemColors.Control;
+            this.b_a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_a1.Location = new System.Drawing.Point(1, 0);
+            this.b_a1.Name = "b_a1";
+            this.b_a1.Size = new System.Drawing.Size(200, 200);
+            this.b_a1.TabIndex = 0;
+            this.b_a1.UseVisualStyleBackColor = false;
+            this.b_a1.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_a2
+            // 
+            this.b_a2.BackColor = System.Drawing.SystemColors.Control;
+            this.b_a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_a2.Location = new System.Drawing.Point(207, 0);
+            this.b_a2.Name = "b_a2";
+            this.b_a2.Size = new System.Drawing.Size(200, 200);
+            this.b_a2.TabIndex = 1;
+            this.b_a2.UseVisualStyleBackColor = false;
+            this.b_a2.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_a3
+            // 
+            this.b_a3.BackColor = System.Drawing.SystemColors.Control;
+            this.b_a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_a3.Location = new System.Drawing.Point(413, 0);
+            this.b_a3.Name = "b_a3";
+            this.b_a3.Size = new System.Drawing.Size(200, 200);
+            this.b_a3.TabIndex = 2;
+            this.b_a3.UseVisualStyleBackColor = false;
+            this.b_a3.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_b1
+            // 
+            this.b_b1.BackColor = System.Drawing.SystemColors.Control;
+            this.b_b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_b1.Location = new System.Drawing.Point(1, 206);
+            this.b_b1.Name = "b_b1";
+            this.b_b1.Size = new System.Drawing.Size(200, 200);
+            this.b_b1.TabIndex = 3;
+            this.b_b1.UseVisualStyleBackColor = false;
+            this.b_b1.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_b2
+            // 
+            this.b_b2.BackColor = System.Drawing.SystemColors.Control;
+            this.b_b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_b2.Location = new System.Drawing.Point(207, 206);
+            this.b_b2.Name = "b_b2";
+            this.b_b2.Size = new System.Drawing.Size(200, 200);
+            this.b_b2.TabIndex = 4;
+            this.b_b2.UseVisualStyleBackColor = false;
+            this.b_b2.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_b3
+            // 
+            this.b_b3.BackColor = System.Drawing.SystemColors.Control;
+            this.b_b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_b3.Location = new System.Drawing.Point(413, 206);
+            this.b_b3.Name = "b_b3";
+            this.b_b3.Size = new System.Drawing.Size(200, 200);
+            this.b_b3.TabIndex = 5;
+            this.b_b3.UseVisualStyleBackColor = false;
+            this.b_b3.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_c1
+            // 
+            this.b_c1.BackColor = System.Drawing.SystemColors.Control;
+            this.b_c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_c1.Location = new System.Drawing.Point(1, 406);
+            this.b_c1.Name = "b_c1";
+            this.b_c1.Size = new System.Drawing.Size(200, 200);
+            this.b_c1.TabIndex = 6;
+            this.b_c1.UseVisualStyleBackColor = false;
+            this.b_c1.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_c2
+            // 
+            this.b_c2.BackColor = System.Drawing.SystemColors.Control;
+            this.b_c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_c2.Location = new System.Drawing.Point(207, 406);
+            this.b_c2.Name = "b_c2";
+            this.b_c2.Size = new System.Drawing.Size(200, 200);
+            this.b_c2.TabIndex = 7;
+            this.b_c2.UseVisualStyleBackColor = false;
+            this.b_c2.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // b_c3
+            // 
+            this.b_c3.BackColor = System.Drawing.SystemColors.Control;
+            this.b_c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_c3.Location = new System.Drawing.Point(413, 406);
+            this.b_c3.Name = "b_c3";
+            this.b_c3.Size = new System.Drawing.Size(200, 200);
+            this.b_c3.TabIndex = 8;
+            this.b_c3.UseVisualStyleBackColor = false;
+            this.b_c3.Click += new System.EventHandler(this.b_a1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(727, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // b_a10
+            // 
+            this.b_a10.Location = new System.Drawing.Point(730, 307);
+            this.b_a10.Name = "b_a10";
+            this.b_a10.Size = new System.Drawing.Size(75, 23);
+            this.b_a10.TabIndex = 10;
+            this.b_a10.Text = "Reboot!";
+            this.b_a10.UseVisualStyleBackColor = true;
+            this.b_a10.Click += new System.EventHandler(this.b_a10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 618);
+            this.Controls.Add(this.b_a10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b_c3);
+            this.Controls.Add(this.b_c2);
+            this.Controls.Add(this.b_c1);
+            this.Controls.Add(this.b_b3);
+            this.Controls.Add(this.b_b2);
+            this.Controls.Add(this.b_b1);
+            this.Controls.Add(this.b_a3);
+            this.Controls.Add(this.b_a2);
+            this.Controls.Add(this.b_a1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_a1;
+        private System.Windows.Forms.Button b_a2;
+        private System.Windows.Forms.Button b_a3;
+        private System.Windows.Forms.Button b_b1;
+        private System.Windows.Forms.Button b_b2;
+        private System.Windows.Forms.Button b_b3;
+        private System.Windows.Forms.Button b_c1;
+        private System.Windows.Forms.Button b_c2;
+        private System.Windows.Forms.Button b_c3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button b_a10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dop5
+{
+    public partial class Form1 : Form
+    {
+        private int player;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            player = 1;
+            label1.Text = "Текущий ход: Игрок 1";
+        }
+
+        public void DisableButtons ()
+        {
+            b_a1.Enabled = false;
+            b_a2.Enabled = false;
+            b_a3.Enabled = false;
+            b_b1.Enabled = false;
+            b_b2.Enabled = false;
+            b_b3.Enabled = false;
+            b_c1.Enabled = false;
+            b_c2.Enabled = false;
+            b_c3.Enabled = false;
+        }
+
+        public void Win(string who) {
+            string message = string.Format("{0} победили!", who);
+            MessageBox.Show(message);
+            DisableButtons();
+        }
+
+        private void b_a1_Click(object sender, EventArgs e)
+        {
+            switch (player)
+            {
+                case 1:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "X");
+                    player = 0;
+                    label1.Text = "Текущий ход: Игрок 2";
+                    break;
+                case 0:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "0");
+                    player = 1;
+                    label1.Text = "Текущий ход: Игрок 1";
+                    break;
+            }
+            sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            checkWin();
+        }
+        private void checkWin()
+        {
+            string who, message;
+            if (b_a1.Text == b_a2.Text && b_a2.Text == b_a3.Text && b_a2.Text != "")
+            {
+                who = b_a2.Text;
+                Win(who);
+                return;
+
+            }
+            if (b_b1.Text == b_b2.Text && b_b2.Text == b_b3.Text && b_b1.Text != "")
+            {
+                who = b_b1.Text;
+                Win(who);
+                return;
+            }
+            if (b_c1.Text == b_c2.Text && b_c2.Text == b_c3.Text && b_c1.Text != "")
+            {
+                who = b_c1.Text;
+                Win(who);
+                return;
+            }
+            if (b_a1.Text == b_b1.Text && b_b1.Text == b_c1.Text && b_b1.Text != "")
+            {
+                who = b_b1.Text;
+                Win(who);
+                return;
+            }
+            if (b_a2.Text == b_b2.Text && b_b2.Text == b_c2.Text && b_a2.Text != "")
+            {
+                who = b_a2.Text;
+                Win(who);
+                return;
+            }
+            if (b_a3.Text == b_b3.Text && b_b3.Text == b_c3.Text && b_a3.Text != "")
+            {
+                who = b_a3.Text;
+                Win(who);
+                return;
+            }
+            if (b_a1.Text == b_b2.Text && b_b2.Text == b_c3.Text && b_b2.Text != "")
+            {
+                who = b_b2.Text;
+                Win(who);
+                return;
+            }
+            if (b_a3.Text == b_b2.Text && b_b2.Text == b_c1.Text && b_a3.Text != "")
+            {
+                who = b_a3.Text;
+                Win(who);
+                return;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b_a10_Click(object sender, EventArgs e)
+        {
+            b_a1.Text = "";
+            b_a2.Text = "";
+            b_a3.Text = "";
+            b_b1.Text = "";
+            b_b2.Text = "";
+            b_b3.Text = "";
+            b_c1.Text = "";
+            b_c2.Text = "";
+            b_c3.Text = "";
+
+            b_a1.Enabled = true;
+            b_a2.Enabled = true;
+            b_a3.Enabled = true;
+            b_b1.Enabled = true;
+            b_b2.Enabled = true;
+            b_b3.Enabled = true;
+            b_c1.Enabled = true;
+            b_c2.Enabled = true;
+            b_c3.Enabled = true;
+
+            player = 1;
+            label1.Text = "Текущий ход: Игрок 1";
+        }
+    }
+}
